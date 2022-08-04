a138f40c449e8353316825eb2b30f1bf3d1be74f
Zeeshan Paracha <zeeshan.paracha@avanzasolutions.com>
2022-08-04 19:31:12 +0500

    organizations get call

--- src/components/Organizer/OrganizerTable/index.jsx
+++ src/components/Organizer/OrganizerTable/index.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import UserDetail from './UserDetails'
+import dateFormat from "dateformat";
 
 import people1 from '../../../assets/images/people1.png'
 import Search from '../../../assets/images/search7.svg'
 
-const OrganizerTable = () => {
+const OrganizerTable = ({ users }) => {
 
     const [modal, setModal] = useState('')
 
-    return(
+    return (
         <div className="org_table">
-            <div className="org_head"><p><span>10</span> Organisateur</p></div>
+            <div className="org_head"><p><span>{users.length}</span> Organisateur</p></div>
             <div className="org_search">
                 <img src={Search} alt="" />
                 <input type="search" placeholder="Chercher" />
@@ -25,62 +26,22 @@ const OrganizerTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr onClick={() => setModal('user')}>
-                        <td><img src={people1} alt=""/></td>
-                        <td>
-                            <p className="org_tableText1">Jane Doe</p>
-                            <p className="org_tableText2">Web Designer</p>
-                        </td>
-                        <td>
-                            <p className="org_tableText1">Luanda</p>
-                        </td>
-                        <td>
-                            <p className="org_tableText1">01-06-2020</p>
-                            <p className="org_tableText2">2:36 PM</p>
-                        </td>
-                    </tr>
-                    <tr onClick={() => setModal('user')}>
-                        <td><img src={people1} alt=""/></td>
-                        <td>
-                            <p className="org_tableText1">Jane Doe</p>
-                            <p className="org_tableText2">Web Designer</p>
-                        </td>
-                        <td>
-                            <p className="org_tableText1">Luanda</p>
-                        </td>
-                        <td>
-                            <p className="org_tableText1">01-06-2020</p>
-                            <p className="org_tableText2">2:36 PM</p>
-                        </td>
-                    </tr>
-                    <tr onClick={() => setModal('user')}>
-                        <td><img src={people1} alt=""/></td>
-                        <td>
-                            <p className="org_tableText1">Jane Doe</p>
-                            <p className="org_tableText2">Web Designer</p>
-                        </td>
-                        <td>
-                            <p className="org_tableText1">Luanda</p>
-                        </td>
-                        <td>
-                            <p className="org_tableText1">01-06-2020</p>
-                            <p className="org_tableText2">2:36 PM</p>
-                        </td>
-                    </tr>
-                    <tr onClick={() => setModal('user')}>
-                        <td><img src={people1} alt=""/></td>
-                        <td>
-                            <p className="org_tableText1">Jane Doe</p>
-                            <p className="org_tableText2">Web Designer</p>
-                        </td>
-                        <td>
-                            <p className="org_tableText1">Luanda</p>
-                        </td>
-                        <td>
-                            <p className="org_tableText1">01-06-2020</p>
-                            <p className="org_tableText2">2:36 PM</p>
-                        </td>
-                    </tr>
+                    {users.map((user => {
+                        return <tr key={user.username} onClick={() => setModal('user')}>
+                            <td><img src={people1} alt="" /></td>
+                            <td>
+                                <p className="org_tableText1">{user.name}</p>
+                                <p className="org_tableText2">{user.profession}</p>
+                            </td>
+                            <td>
+                                <p className="org_tableText1">Luanda</p>
+                            </td>
+                            <td>
+                                <p className="org_tableText1">{dateFormat(user.createdAt, "paddedShortDate")}</p>
+                                <p className="org_tableText2">{dateFormat(user.createdAt, "shortTime")}</p>
+                            </td>
+                        </tr>
+                    }))}
                 </tbody>
             </table>
             {modal === 'user' && <UserDetail setModal={setModal} />}
--- src/pages/Organizer/index.jsx
+++ src/pages/Organizer/index.jsx
@@ -1,25 +1,39 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import SideBar from "../../components/ReuseableComponents/Sidebar"
 import Header from "../../components/ReuseableComponents/Header"
 import OrganizerTable from "../../components/Organizer/OrganizerTable"
 import AddOrganizer from "../../components/Organizer/AddOrganizer"
+import instance from "../../instance"
 
 const Organizer = () => {
 
     const [activeTab, setActiveTab] = useState(1)
+    const [users, setUsers] = useState([])
 
-    return(
+    useEffect(() => {
+        instance.get('organization/users')
+            .then(res => {
+                setUsers(res.data.data)
+            })
+            .catch(err => {
+                if (err.response.data.error) {
+                    console.log(err.response.data.error)
+                }
+            })
+    }, [])
+
+    return (
         <div className="org">
             <SideBar index={2} />
             <div className="org_body">
-                <Header/>
+                <Header />
                 <div className="org_tabs">
                     <p className={activeTab === 1 ? 'org_tabName active' : 'org_tabName'} onClick={() => setActiveTab(1)}>Organisateurs</p>
                     <p className={activeTab === 2 ? 'org_tabName active' : 'org_tabName'} onClick={() => setActiveTab(2)}>Enregistrer un organisateur</p>
                 </div>
-                {activeTab === 1 && <OrganizerTable/>}
-                {activeTab === 2 && <AddOrganizer/>}
+                {activeTab === 1 && <OrganizerTable users={users} />}
+                {activeTab === 2 && <AddOrganizer />}
             </div>
         </div>
     )
